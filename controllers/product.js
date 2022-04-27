e11664bda1eb4ed9a264d09595b3da1286ec37ef
yetabalij <ermiastsegu@yahoo.com>
2022-04-27 21:48:54 +0300

    getProducts controllers is added.

--- controllers/product.js
+++ controllers/product.js
@@ -1,9 +1,27 @@
 const Product = require("./../models/Product");
 
+//create new product
 exports.newProduct = async (req, res, next) => {
-  const product = await Product.create(req.body);
-  res.status(201).json({
-    success: true,
-    product,
-  });
+  try {
+    const product = await Product.create(req.body);
+    res.status(201).json({
+      success: true,
+      product,
+    });
+  } catch (error) {
+    res.status(400).json({ message: "something went wrong" });
+  }
+};
+
+//get all products
+exports.getProducts = async (req, res, next) => {
+  try {
+    const products = await Product.find();
+    res.status(200).json({
+      success: true,
+      products,
+    });
+  } catch (error) {
+    res.status(400).json({ message: "something went wrong" });
+  }
 };
