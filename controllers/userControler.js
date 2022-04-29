f50856b4d2723a738d42b04e2a071d1799d2c17c
yetabalij <ermiastsegu@yahoo.com>
2022-04-29 16:08:34 +0300

    every thing about user is deleted.

--- controllers/userControler.js
+++
@@ -1,5 +0,0 @@
-const User = require("../models/userModels");
-
-exports.register = (req, res) => {
-  res.send("Hello Ermi");
-};
--- routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-
-const { register } = require("./../controllers/userControler");
-
-const route = express.Router();
-route.get("/register", register);
-
-module.exports = route;
