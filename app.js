a6d5c4d856fd96c1c844886e7cac45eb14c7b770
yetabalij <ermiastsegu@yahoo.com>
2022-04-29 16:45:01 +0300

    mongoose installed and db connected.

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+//app
 const app = express();
-require("dotenv").config;
+
+// //db
+
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("database connected");
+  });
 
 app.get("/", (req, res) => {
   res.send("hello");
