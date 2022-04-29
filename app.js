7eb47d79ebe237a914deb494a26091a432f1b98d
yetabalij <ermiastsegu@yahoo.com>
2022-04-29 16:53:03 +0300

    a basic route is created.

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
+//import routes
+const userRoutes = require("./routes/user");
 
 //app
 const app = express();
@@ -16,9 +18,8 @@ mongoose
     console.log("database connected");
   });
 
-app.get("/", (req, res) => {
-  res.send("hello");
-});
+//routes middleware
+app.use("/api", userRoutes);
 
 const port = process.env.PORT || 5000;
 
