e2e3a6dbecc0fa80e32b9f0e4b85583eb9010368
yetabalij <ermiastsegu@yahoo.com>
2022-05-05 17:28:53 +0300

    redux slice and store is configured.

---
+++ client/src/redux/store.js
@@ -0,0 +1,6 @@
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "../redux/features/counterslice";
+
+export default configureStore({
+  //reducer: { counter: counterReducer },
+});
